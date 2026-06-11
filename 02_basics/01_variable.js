119f0c07dc90b7015282fac0c338b5a90fd83c16
Ishan Mandal <111856580+ishan8471@users.noreply.github.com>
2026-06-11 13:52:48 +0000

    container config

---
+++ 02_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 1223
+let accountemail = "er.ishan10763@gmail.com"
+var accountPassword = "789"
+accountCity = "Jaipur"
+let accountState;
+
+//let is mostly perfered instead of var
+/* var is not used in Js because of issue in block
+ scope and functional scope */
+
+
+accountemail = "mandalishan96@gmail.com"
+accountPassword = "10763"
+accountCity = "Mirchaiya"
+
+
+
+console.log(accountId) // single print method
+console.table([accountId, accountemail, accountPassword, accountCity, accountState]);
---
+++ 02_basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer verson
+
+//alert( 3 + 3) //we are using nodejs, not browser
+
+console.log( 3 * 3)  // code readability should be high
+console.log("Ishan")
+
+
+let name = "Ishan" // string 
+let age = 25 // number => 2 to the power 53
+let isLoggedIn = false // BOOLEAN
+
+
+/* DATATYPE */
+//bigint 
+// BOOLEAN => TRUE / FALSE
+//null => standalone Value 
+// ndefined => 
+// symbol => mostly used for uniqueness
+//object 
+
+
+console.log(typeof age)
+console.log(typeof "Ishan");
+console.log(typeof null);
